26ff532ecc6cce90695e194bd979582279be7d30
Malcolm Roberts <malcolmroberts@amd.com>
2019-06-07 10:49:08 -0600

    Fix first element for CS_KERNEL_R_TO_CMPLX reference code.

--- library/src/include/ref_cpu.h
+++ library/src/include/ref_cpu.h
@@ -668,7 +668,8 @@ class RefLibOp
             {
                 const auto bin  = input + ibatch * halfN;
                 auto       bout = output + ibatch * (halfN + 1);
-                for(int r = 0; r < halfN; ++r)
+                bout[0] = std::complex<float>(bin[0].real() + bin[0].imag());
+                for(int r = 1; r < halfN; ++r)
                 {
                     const auto omegaNr
                         = std::exp(std::complex<float>(0.0f, (float)(-2.0f * M_PI * r * overN)));
