ccb9e6428a3027e4a3252bec0403f43daf496f38
Malcolm Roberts <malcolm.roberts@amd.com>
2019-07-04 13:27:41 -0600

    Remove FFTW dependency for hipfft examples and simplify examples. (#212)
    * Remove FFTW dependency for hipfft examples and simplify examples.

--- clients/samples/hipfft/hipfft_setworkarea.cpp
+++ clients/samples/hipfft/hipfft_setworkarea.cpp
@@ -20,10 +20,9 @@
 * THE SOFTWARE.
 *******************************************************************************/
 
-#include <iomanip>
+#include <complex>
 #include <iostream>
 
-#include <fftw3.h>
 #include <hip/hip_runtime.h>
 #include <hipfft.h>
 
@@ -35,88 +34,65 @@ int main()
     hipfftGetProperty(MAJOR_VERSION, &major_version);
     std::cout << "hipFFT major_version " << major_version << std::endl;
 
-    const size_t N = 256;
+    const size_t N        = 9;
+    const size_t Ncomplex = (N / 2 + 1);
 
-    /// FFTW reference compute
+    std::vector<float>               rdata(N);
+    std::vector<std::complex<float>> cdata(Ncomplex);
 
-    std::vector<float2> cx(N);
+    size_t real_bytes    = sizeof(decltype(rdata)::value_type) * rdata.size();
+    size_t complex_bytes = sizeof(decltype(cdata)::value_type) * cdata.size();
 
-    size_t complex_input_bytes = sizeof(fftwf_complex) * (N / 2 + 1);
-    size_t real_output_bytes   = sizeof(float) * N;
-
-    fftwf_complex* in  = (fftwf_complex*)fftwf_malloc(complex_input_bytes);
-    float*         out = (float*)fftwf_malloc(real_output_bytes);
-    fftwf_plan     p   = fftwf_plan_dft_c2r_1d(N, in, out, FFTW_ESTIMATE);
-
-    for(size_t i = 0; i < (N / 2 + 1); i++)
+    std::cout << "input:\n";
+    for(size_t i = 0; i < N; i++)
     {
-        cx[i].x = in[i][0] = i + (i % 3) - (i % 7);
-        cx[i].y = in[i][1] = 0;
+        rdata[i] = i;
     }
-
-    fftwf_execute(p);
-
-    /// hipfft gpu compute
+    for(size_t i = 0; i < N; i++)
+    {
+        std::cout << rdata[i] << " ";
+    }
+    std::cout << std::endl;
 
     // Create HIP device object.
-    hipfftComplex* x;
-    hipMalloc(&x, std::max(complex_input_bytes, real_output_bytes));
+    hipfftReal* x;
+    hipMalloc(&x, real_bytes);
+    hipfftComplex* y;
+    hipMalloc(&y, complex_bytes);
 
-    //  Copy input data to device
-    hipMemcpy(x, &cx[0], complex_input_bytes, hipMemcpyHostToDevice);
+    // Copy input data to device
+    hipMemcpy(x, rdata.data(), real_bytes, hipMemcpyHostToDevice);
 
-    // Create plan
+    size_t workSize;
+    hipfftEstimate1d(N, HIPFFT_R2C, 1, &workSize);
+    std::cout << "hipfftEstimate 1d workSize: " << workSize << std::endl;
+
     hipfftHandle plan = NULL;
-    size_t       workSize;
-
-    hipfftEstimate1d(N, HIPFFT_C2R, 1, &workSize);
-    std::cout << "hipfftEstimate1d workSize: " << workSize << std::endl;
-
     hipfftCreate(&plan);
     hipfftSetAutoAllocation(plan, 0);
-
-    hipfftMakePlan1d(plan, N, HIPFFT_C2R, 1, &workSize);
+    hipfftMakePlan1d(plan, N, HIPFFT_R2C, 1, &workSize);
 
     // Set work buffer
     hipfftComplex* workBuf;
     hipMalloc(&workBuf, workSize);
     hipfftSetWorkArea(plan, workBuf);
-
     hipfftGetSize(plan, &workSize);
     std::cout << "hipfftGetSize workSize: " << workSize << std::endl;
 
     // Execute plan
-    hipfftExecC2R(plan, x, (hipfftReal*)x);
+    hipfftExecR2C(plan, x, (hipfftComplex*)y);
 
     // Copy result back to host
-    std::vector<float> y(N);
-    hipMemcpy(&y[0], x, real_output_bytes, hipMemcpyDeviceToHost);
+    hipMemcpy(cdata.data(), y, complex_bytes, hipMemcpyDeviceToHost);
 
-    // Destroy plan
-    hipfftDestroy(plan);
-
-    double error      = 0;
-    size_t element_id = 0;
-    for(size_t i = 0; i < N; i++)
+    std::cout << "output:\n";
+    for(size_t i = 0; i < Ncomplex; i++)
     {
-        if(i < 32)
-            printf("element %d: FFTW result %f; hipFFT result %f \n", (int)i, out[i], y[i]);
-        double err = fabs(out[i] - y[i]);
-        if(err > error)
-        {
-            error      = err;
-            element_id = i;
-        }
+        std::cout << cdata[i] << " ";
     }
+    std::cout << std::endl;
 
-    printf("......\nmax error of FFTW and hipFFT is %e at element %d\n",
-           error / fabs(out[element_id]),
-           (int)element_id);
-
-    fftwf_destroy_plan(p);
-    fftwf_free(in);
-    fftwf_free(out);
-
+    hipfftDestroy(plan);
     hipFree(x);
     hipFree(workBuf);
 
