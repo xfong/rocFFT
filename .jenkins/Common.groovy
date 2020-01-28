4deb6d937d0231cef8085998fb19f4323c11e3df
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2020-01-28 16:08:50 -0700

    Debug Jenkinsfile for CI (#90)

--- .jenkins/Common.groovy
+++ .jenkins/Common.groovy
@@ -1,12 +1,13 @@
 // This file is for internal AMD use.
 // If you are interested in running your own Jenkins, please raise a github issue for assistance.
 
-def runCompileCommand(platform, project)
+def runCompileCommand(platform, project, boolean debug=false)
 {
     project.paths.construct_build_prefix()
 
     String compiler = platform.jenkinsLabel.contains('hip-clang') ? 'hipcc' : 'hcc'
     String clientArgs = '-DBUILD_CLIENTS_SAMPLES=ON -DBUILD_CLIENTS_TESTS=ON -DBUILD_CLIENTS_BENCHMARKS=ON -DBUILD_CLIENTS_SELFTEST=ON -DBUILD_CLIENTS_RIDER=ON'
+    String buildTypeArg = debug ? '-DCMAKE_BUILD_TYPE=Debug' : '-DCMAKE_BUILD_TYPE=Release'
     String hipClangArgs = platform.jenkinsLabel.contains('hip-clang') ? '-DUSE_HIP_CLANG=ON -DHIP_COMPILER=clang' : ''
     String cmake = platform.jenkinsLabel.contains('centos') ? 'cmake3' : 'cmake'
     String sudo = platform.jenkinsLabel.contains('sles') ? 'sudo' : ''
@@ -15,7 +16,7 @@ def runCompileCommand(platform, project)
                 set -x
                 cd ${project.paths.project_build_prefix}
                 mkdir build && cd build
-                ${sudo} ${cmake} -DCMAKE_CXX_COMPILER=/opt/rocm/bin/${compiler} ${clientArgs} ${hipClangArgs} ..
+                ${sudo} ${cmake} -DCMAKE_CXX_COMPILER=/opt/rocm/bin/${compiler} ${buildTypeArg} ${clientArgs} ${hipClangArgs} ..
                 ${sudo} make -j\$(nproc)
             """
     platform.runCommand(this, command)
