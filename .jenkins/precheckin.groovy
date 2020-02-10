ed64d32cd4f915a02c728031f4ed6e46abc018c2
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2020-02-10 11:30:14 -0700

    Fix label and jobname check for precheckin CI jobs (#99)

--- .jenkins/precheckin.groovy
+++ .jenkins/precheckin.groovy
@@ -13,7 +13,7 @@ def runCI =
 {
     nodeDetails, jobName->
 
-    def prj  = new rocProject('rocFFT-internal', 'Debug')
+    def prj  = new rocProject('rocFFT-internal', 'PreCheckin')
 
     // Define test architectures, optional rocm version argument is available
     def nodes = new dockerNodes(nodeDetails, jobName, prj)
@@ -76,9 +76,8 @@ ci: {
             }
     }
 
-    // For url job names that are outside of the standardJobNameSet i.e. compute-rocm-dkms-no-npi-1901
-    Set standardJobNameSet = ["compute-rocm-dkms-no-npi", "compute-rocm-dkms-no-npi-hipclang", "rocm-docker"]
-    if(!standardJobNameSet.contains(urlJobName))
+    // For url job names that are not listed by the jobNameList i.e. compute-rocm-dkms-no-npi-1901
+    if(!jobNameList.keySet().contains(urlJobName))
     {
         properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * *')])]))
         stage(urlJobName) {
